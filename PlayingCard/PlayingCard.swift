5905a8d5e731ad3e0b3d881954acb82eeb06bdfc
Kristopher Pasillas <kris@kris-p.net>
2018-12-15 19:17:38 -0800

    Initial commit (Lecture #5)

---
+++ PlayingCard/PlayingCard.swift
@@ -0,0 +1,57 @@
+//
+//  PlayingCard.swift
+//  PlayingCard
+//
+//  Created by Work Kris on 12/12/18.
+//  Copyright © 2018 Kris P. All rights reserved.
+//
+
+import Foundation
+
+struct PlayingCard: CustomStringConvertible
+{
+    var description: String { return "\(rank)\(suit)"}
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+        var description: String { return "Suit1" }
+        
+        case spades = "♠️"
+        case hearts = "❤️"
+        case clubs = "♣️"
+        case diamonds = "♦️"
+        
+        static var all = [Suit.spades, .hearts, .clubs, .diamonds]
+    }
+    
+    enum Rank: CustomStringConvertible {
+        var description: String  { return "Rank1" }
+        
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default: return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRanks = [Rank.ace]
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            allRanks += [Rank.face("J"), .face("Q"), .face("K")]
+            
+            return allRanks
+        }
+    }
+}
---
+++ PlayingCard/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Work Kris on 12/12/18.
+//  Copyright © 2018 Kris P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+    }
+
+
+}
+
